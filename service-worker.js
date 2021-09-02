5aabdf4e1dd0e135e8e0c5525c1a20ec262cc210
floysh <floysh@outlook.it>
2021-09-03 00:35:05 +0200

    PWA service worker and webmanifest

---
+++ service-worker.js
@@ -0,0 +1,79 @@
+
+
+// Delete the old app cached files
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+            return true;
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          }),
+      );
+    }),
+  );
+});
+
+
+// Cache the app main files
+const filesToCache = [
+    'style.css',
+    'bundle.js',
+    'index.html',
+    "assets/icons/icon-128x128.png",
+    "assets/icons/icon-152x152.png",
+    "assets/icons/icon-384x384.png",
+    "assets/icons/icon-512x512.png",
+    "assets/icons/icon-96x96.png",
+    "assets/icons/icon-144x144.png",
+    "assets/icons/icon-192x192.png",
+    "assets/icons/icon-48x48.png",
+    "assets/icons/icon-72x72.png",
+  ];
+  
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => {
+        return cache.addAll(filesToCache);
+        })
+    );
+});
+
+
+// Cache the valueset .json files
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+        if (response) {
+            console.log('Found ', event.request.url, ' in cache');
+            return response;
+        }
+
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+          .then(response => {
+              return caches.open(staticCacheName).then(cache => {
+                cache.put(event.request.url, response.clone());
+                return response;
+              });
+            });
+
+          }).catch(error => {
+
+          // TODO 6 - Respond with custom offline page
+
+          })
+    );
+});
