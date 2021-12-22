e031d0a39862924d4e9c4760347e6e86ab9f4697
floysh <floysh@outlook.it>
2021-12-22 17:53:54 +0100

    updated signing certificates and public keys (22/12/2021)

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Change this number to force a new service worker install
 // and app update
-const SERVICE_WORKER_VERSION = 118
+const SERVICE_WORKER_VERSION = 119
 
 // Delete the old app cached files
 self.addEventListener('activate', function (event) {
