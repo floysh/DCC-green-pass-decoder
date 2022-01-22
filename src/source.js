2202443170eae0bb94ea27f8d1d9f44b21665a93
floysh <floysh@outlook.it>
2022-01-22 16:05:09 +0100

    disabled scan button on HTTP
    optimized fontawesome import
    
    updated signing certificates and public keys (22/01/2022)

--- src/source.js
+++ src/source.js
@@ -163,15 +163,29 @@ const qrScanner = new QrScanner(UI.scannerVideo, rawstring => {
 	});
 });
 
-document.getElementById("start-scan").addEventListener("click", event => {
-	UI.reset();
-	UI.hideQRCanvas();
-	UI.scanner.hidden = false;
-	UI.setProgressText("Awaiting for scan results")
-	qrScanner.start()
-	.catch(err => {
-		alert(err+"\nThe camera stream is only available on HTTPS");
-		UI.scanner.hidden = false;
+// Scan button
+window.addEventListener("load", event => {
+	const scanButton = document.getElementById("start-scan");
+	scanButton.addEventListener("click", event => {
+		if (location.protocol !== "https:") {
+			// Disable scan if the page is not loaded through https
+			scanButton.disabled = true;
+			scanButton.classList.add("is-static")
+			alert("This feature is only available on HTTPS");
+			return -1;
+		}
+		UI.setProgressText("Loading scanner");
+		qrScanner.start()
+		.then( () => {
+			UI.reset();
+			UI.hideQRCanvas();
+			UI.scanner.hidden = false;
+			UI.setProgressText("Awaiting for scan results");
+		})
+		.catch(err => {
+			UI.hideProgress();
+			alert(err+"\n\nNote: If this device has a working camera, you may need to grant camera access to this page. Check the site permissions. Camera streams also require an HTTPS connection");
+		})
 	})
 })
 
