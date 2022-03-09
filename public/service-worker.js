05d36a2844be52311f147516eca80b4e622ddd96
floysh <floysh@outlook.it>
2022-03-09 11:32:57 +0100

    updated signing certificates and public keys (09/03/2022)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Change this number to force a new service worker install
 // and app update
-const SERVICE_WORKER_VERSION = 133
+const SERVICE_WORKER_VERSION = 134
 
 // Cache the app main files
 const filesToCache = [
